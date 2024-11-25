45e98ab8701be4d8924be5cbd511a8c78b497af5
Igor Rafael <igor.rafael.briano@gmail.com>
2024-11-25 15:39:26 -0300

    adicionado verificacao de idade // criado funcoes para cadastro do carro

--- Questao C/CadastroPessoa.java
+++ Questao C/CadastroPessoa.java
@@ -1,3 +1,7 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class CadastroPessoa {
     private String nome;
     private String email;
@@ -19,4 +23,36 @@ public class CadastroPessoa {
     public String getDataNasc(){ return this.dataNasc; }
 
     public String getTelefone(){ return this.telefone; }
+
+    //validação segue exemplo de https://www.guj.com.br/t/resolvido-validacao-de-data/339866/3
+    public boolean validaData(String data){
+        try{
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            sdf.setLenient(false);
+            sdf.parse(data);
+            return true;
+        }catch (ParseException ex) {
+            return false;
+        }
+    }
+    
+    public int calculoIdade(String data){
+        try{
+            int dia = Integer.parseInt(data.substring(0,2));
+            int mes = Integer.parseInt(data.substring(3,5));
+            int ano = Integer.parseInt(data.substring(6,10));
+    
+            Calendar cHoje= Calendar.getInstance();
+            int idade = cHoje.get(Calendar.YEAR) - ano;
+    
+            if(cHoje.get(Calendar.MONTH) >= mes && cHoje.get(Calendar.DAY_OF_MONTH) > dia) idade += 1;
+    
+            return idade;
+        }catch(Error e){
+            System.out.println("Algo aconteceu, não foi possivel prosseguir com o cadastro");
+            System.out.println(e.getMessage());
+
+            return 0;
+        }
+    }
 }
--- Questao C/QuestaoC.java
+++ Questao C/QuestaoC.java
@@ -3,32 +3,11 @@ import java.util.Scanner;
 
 public class QuestaoC{
     static Scanner sc = new Scanner(System.in);
+    static CadastroPessoa cadPessoa = new CadastroPessoa();
 
     public static void main(String[] args){
-        int escolha;
         try{
-            do{
-                System.out.print("Iniciado processo de cadastro");
-                System.out.print("\n 1 - cadastro de pessoa");
-                System.out.print("\n 2 - cadastro de carro");
-                System.out.print("\n 3 - Sair\n");
-                System.out.print("Informe o item desejado: "); escolha = sc.nextInt();
-                if(escolha < 1 || escolha > 3){
-                    System.out.println("Por favor, escolha umas das opções validas do menu");
-                }else{
-                    switch (escolha) {
-                        case 1:
-                            cadastroPessoa();
-                            break;
-                        case 2:
-                            System.out.println("você escolheu o 2");
-                            break;
-                        case 3:
-                            System.out.println("você escolheu o 3");
-                            break;
-                    }
-                }
-            }while(escolha < 1 || escolha > 3);
+            cadastroPessoa();
         }catch(InputMismatchException e){
             sc.close();
             System.out.println("Ops... você digitou caracteres. Precisamos que digite apenas números.");
@@ -36,14 +15,27 @@ public class QuestaoC{
     }
 
     public static void cadastroPessoa(){
-        CadastroPessoa cadPessoa = new CadastroPessoa();
+        
+        try{
+            System.out.println("Iniciado processo para cadastro de pessoa.");
+            cadPessoa.setNome(nomePessoa());
+            cadPessoa.setEmail(emailPessoa());
+            cadPessoa.setDataNasc(nascPessoa());
+            cadPessoa.setTelefone(telPessoa());
+            System.out.println(cadPessoa.getNome() + " Cadastrado(a).");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
-        System.out.println("Iniciado processo para cadastro de pessoa.");
-        cadPessoa.setNome(nomePessoa());
-        cadPessoa.setEmail(emailPessoa());
-        cadPessoa.setDataNasc(nascPessoa());
+    public static void cadastroCarro(){
+        CadastroCarro cadCarro = new CadastroCarro();
 
-        System.out.println(cadPessoa.getEmail());
+        System.out.println("Iniciado processo para cadastro do carro.");
+        cadCarro.setmarca(marcaCarro());
+        cadCarro.setmodelo(modeloCarro());
+        cadCarro.setplaca(placaCarro());
+        System.out.println(cadCarro.getmodelo() + " Cadastrado.");
     }
 
     public static String nomePessoa(){
@@ -91,27 +83,118 @@ public class QuestaoC{
         return email;
     }
 
-    public static String nascPessoa(){
-        boolean dataSimb;
+    public static String nascPessoa() throws Exception{
+        boolean davaValid;
         String data = "";
-
         try{
             do{
-                dataSimb = false;
                 System.out.print("Informe data de nascimento dd/mm/aaaa: ");
                 data = sc.nextLine();
-                if(!data.matches("[0-3][0-9][/][0-1][1-2][/][0-9]{4}")){
-                    dataSimb = true;
+                davaValid = cadPessoa.validaData(data);
+                if(!davaValid){
                     System.out.println("Por favor, insira uma data valida.");
                 }
-            }while(dataSimb);
+            }while(!davaValid);
+            if(cadPessoa.calculoIdade(data) < 18){
+                throw new Exception("A pessoa a ser cadastrada é menor de idade");
+            }
         }catch(Error e){
             System.out.println("Algo aconteceu, não foi possivel prosseguir com o cadastro");
             System.out.println(e.getMessage());
-        }finally{
             sc.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
 
         return data;
+    }
+
+    public static String telPessoa(){
+        boolean telSimb;
+        String telefone = "";
+
+        try{
+            do{
+                telSimb = false;
+                System.out.print("Informe o telefone (11)91234-5678: ");
+                telefone = sc.nextLine();
+                if(!telefone.matches("[(][0-9]{2}[)][9][0-9]{4}[-][0-9]{4}")){
+                    telSimb = true;
+                    System.out.println("Por favor, insira um numero valido conforme o indicado.");
+                }
+            }while(telSimb);
+        }catch(Error e){
+            System.out.println("Algo aconteceu, não foi possivel prosseguir com o cadastro");
+            System.out.println(e.getMessage());
+            sc.close();
+        }
+
+        return telefone;
+    }
+
+    public static String marcaCarro(){
+        boolean marcaVerif;
+        String marca = "";
+
+        try{
+            do{
+                marcaVerif = false;
+                System.out.print("Informe a marca: ");
+                marca = sc.nextLine();
+                if(!marca.matches("[A-z]")){
+                    marcaVerif = true;
+                    System.out.println("Informe uma marca valida");
+                }
+            }while(marcaVerif);
+        }catch(Error e){
+            System.out.println("Algo aconteceu, não foi possivel prosseguir com o cadastro");
+            System.out.println(e.getMessage());
+            sc.close();
+        }
+        return marca;
+    }
+
+    public static String modeloCarro(){
+        boolean modeloVerif;
+        String modelo = "";
+
+        try{
+            do{
+                modeloVerif = false;
+                System.out.print("Informe o modelo: ");
+                modelo = sc.nextLine();
+                if(!modelo.matches("[A-z]")){
+                    modeloVerif = true;
+                    System.out.println("Informe um modelo valida");
+                }
+            }while(modeloVerif);
+        }catch(Error e){
+            System.out.println("Algo aconteceu, não foi possivel prosseguir com o cadastro");
+            System.out.println(e.getMessage());
+            sc.close();
+        }
+        return modelo;
+    }
+
+    public static String placaCarro(){
+        boolean marcaVerif;
+        String marca = "";
+
+        try{
+            do{
+                marcaVerif = false;
+                System.out.print("Informe a placa: ");
+                marca = sc.nextLine();
+                if(!marca.matches("[A-Z]{3}[-][0-9]{4}") || !marca.matches("[A-Z]{3}[0-9][A-Z][0-9]{2}")){
+                    marcaVerif = true;
+                    System.out.println("Informe uma placa valida");
+                }
+            }while(marcaVerif);
+        }catch(Error e){
+            System.out.println("Algo aconteceu, não foi possivel prosseguir com o cadastro");
+            System.out.println(e.getMessage());
+            sc.close();
+        }
+        return marca;
     }
 }
